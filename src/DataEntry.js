e1b2ad8cddd2d25f2eb3a03b7f4f5551817527b2
James Hirschfelder <james.hirschfelder@gmail.com>
2021-08-12 17:02:40 -0700

    Captured state of input fields

--- src/DataEntry.js
+++ src/DataEntry.js
@@ -2,29 +2,67 @@ import React from 'react'
 
 
 class DataEntry extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      expensename: "",
+      date: "",
+      expensetype: "",
+      amount: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
     
     return (
-      <div>
+      <form>
         <span>Expense name:</span>
-          <input type="text" name="expense-name" placeholder="Enter reference name"/>
+          <input 
+            type="text" 
+            value={this.state.expensename} 
+            name="expensename" 
+            placeholder="Enter reference name"
+            onChange={this.handleChange}
+          />
   
         <span>Date:</span>
-          <input type="date" name="date"/>
+          <input 
+            type="date" 
+            value={this.state.date} 
+            name="date"
+            onChange={this.handleChange}
+          />
   
         <span>Expense type:</span>
-          <select name="expense-type">
-            <option value="debit">Debit</option>
-            <option value="credit">Credit</option>
-            <option value="cash">Cash</option>
-            <option value="crypto">Crypto</option>
-            <option value="other">Other</option> bgtyh
+          <select 
+            value={this.state.expensetype}
+            name="expensetype"
+            onChange={this.handleChange}
+          >
+              <option value="debit">Debit</option>
+              <option value="credit">Credit</option>
+              <option value="cash">Cash</option>
+              <option value="crypto">Crypto</option>
+              <option value="other">Other</option>
           </select>
   
         <span>Amount:</span>
-          <input type="number" name="expense-amount" placeholder="0.00"/>
-      </div>
+          <input 
+            type="number" 
+            value={this.state.amount} 
+            name="amount" 
+            placeholder="0.00"
+            onChange={this.handleChange}
+          />
+          <h1>{this.state.expensename} {this.state.date} {this.state.expensetype} {this.state.amount}</h1>
+      </form>
     )
 
   }
